bbc820da895ac0271efde7e5d386b83f118d6128
Tayo <92905932+tayo6@users.noreply.github.com>
2026-07-06 20:08:24 +0100

    Update PingPongButton.cpp

--- Source/Components/PingPongButton.cpp
+++ Source/Components/PingPongButton.cpp
@@ -19,8 +19,8 @@ void PingPongButton::paint(juce::Graphics& g)
     g.setColour(juce::Colour(0xFFcccccc));
     g.setFont(juce::Font("Arial", 10, juce::Font::plain));
 
-    juce::String text = getToggleState() ? "ON" : "OFF";
-    g.drawText(text, bounds, juce::Justification::centred, true);
+    juce::String displayText = getToggleState() ? "ON" : "OFF";
+    g.drawText(displayText, bounds, juce::Justification::centred, true);
 
     g.setColour(juce::Colour(0xFF444444));
     g.drawRect(bounds);
