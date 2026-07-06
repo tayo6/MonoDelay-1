befc469d1fb5d3fe6983ba7aff4a42bfb0aa7478
Tayo <92905932+tayo6@users.noreply.github.com>
2026-07-06 18:58:47 +0100

    Create Components/RotaryKnob.h

---
+++ Source/Components/RotaryKnob.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <juce_gui_basics/juce_gui_basics.h>
+
+class RotaryKnob : public juce::Slider
+{
+public:
+    RotaryKnob();
+    ~RotaryKnob() override;
+
+    void paint(juce::Graphics& g) override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RotaryKnob)
+};
